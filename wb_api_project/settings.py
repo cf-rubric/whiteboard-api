82786359690e18a6f582dcff1ed04cc9bc49f221
MasonChance <mason.fryberger@gmail.com>
2021-05-12 21:02:38 -0700

    docker is tested and running

--- wb_api_project/settings.py
+++ wb_api_project/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = tuple(env('ALLOWED_HOSTS'))
+ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS'))
 
 
 # Application definition
