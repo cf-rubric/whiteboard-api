cceb240f069735fd6dfb817ee3a4b695de4bd658
MasonChance <mason.fryberger@gmail.com>
2021-05-13 16:52:54 -0700

    permissions and view_permissions altered

--- wb_app/permissions.py
+++ wb_app/permissions.py
@@ -1,7 +1,5 @@
-import rest_framework
-from rest_framework import permissions
+# import rest_framework
+# from rest_framework import permissions
+# from django.contrib.auth.models import User
 # from django.contrib.auth import get_user_model
 
-class isAuth(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return super().has_permission(request, view)
